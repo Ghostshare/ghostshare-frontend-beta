fb511916c0c790d3272c8de25b362fac1cbaa913
Joris Zierold <contact@joriszierold.com>
2022-07-14 13:08:26 +0100

    add ghost emoji as favicon

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
         <title>Ghostshare</title>
         <meta name="description" content="Private File Transfer. Simplified." />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👻</text></svg>">
       </Head>
 
       <main className={styles.main}>
