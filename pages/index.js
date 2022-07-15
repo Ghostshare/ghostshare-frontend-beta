045b293590ae0c71e08b9cc123eda66c02e92c52
Joris Zierold <contact@joriszierold.com>
2022-07-15 20:25:42 +0100

    add MUI basic setup

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
+import { Typography } from "@mui/material";
+import { Container } from "@mui/system";
 import Head from "next/head";
+import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Ghostshare</title>
         <meta name="description" content="Private File Transfer. Simplified." />
@@ -12,13 +15,14 @@ export default function Home() {
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👻</text></svg>"
         ></link>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="#">Ghostshare! 👻</a>
-        </h1>
-      </main>
-
+      <Navbar />
+      <Container className={styles.main}>
+        <Typography variant="h2" style={{ whiteSpace: "pre-line" }}>
+          {`Private 
+          File-Sharing. 
+          Simplified.`}
+        </Typography>
+      </Container>
       <footer className={styles.footer}>Created with 🖤</footer>
     </div>
   );
