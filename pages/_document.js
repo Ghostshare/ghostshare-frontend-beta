9437bfcf3dd5f86ab81501212e1f663a94cc8516
Joris Zierold <contact@joriszierold.com>
2022-07-16 03:05:55 +0100

    fixed typo crossOrigin

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ export default class MyDocument extends Document {
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link rel="shortcut icon" href="/static/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;500;700&family=Quicksand:wght@400;700&display=swap"
             rel="stylesheet"
