df7c7569c1482ae5188e7c566521696443341de4
Joris Zierold <contact@joriszierold.com>
2022-07-19 21:45:47 +0100

    add event logs overview for testing

---
+++ pages/MetaTx/EventLog.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import { ethers } from "ethers";
+import contracts from "../../metadata/deployed_contracts.json";
+import { FileRegistry } from "../../metadata/contracts_ABI.json";
+
+export default function EventLog({ provider }) {
+  console.log({ provider });
+
+  const [registerEvents, setRegisterEvents] = useState([]);
+  const [accessEvents, setAccessEvents] = useState([]);
+  const [startBlock, setStartBlock] = useState(27244544);
+  const [endBlock, setEndBlock] = useState(27248034);
+
+  const queryEvents = () => {
+    if (provider?._isProvider) {
+      console.log("EventListener");
+      console.log({ provider });
+      const fileRegistryContract = new ethers.Contract(
+        contracts.FileRegistry,
+        FileRegistry,
+        provider
+      );
+
+      fileRegistryContract
+        .queryFilter("FileRegistered", startBlock, endBlock)
+        .then((events) => setRegisterEvents(events));
+      fileRegistryContract
+        .queryFilter("AccessGranted", startBlock, endBlock)
+        .then((events) => setAccessEvents(events));
+    }
+  };
+
+  return (
+    <>
+      <h2>Event Logs</h2>
+      <div>
+        <label>Query from startBlock</label>
+        <input
+          value={startBlock}
+          placeholder="Query from startBlock"
+          style={{ width: "200px" }}
+          onChange={(event) => setStartBlock(Number(event.target.value))}
+        />
+        <br />
+        <label>Query to endBlock</label>
+        <input
+          value={endBlock}
+          placeholder="Query to endBlock"
+          style={{ width: "200px" }}
+          onChange={(event) => setEndBlock(Number(event.target.value))}
+        />
+        <br />
+        <button onClick={queryEvents}>Query Events</button>
+      </div>
+      <h4>FileRegistered</h4>
+      {registerEvents && (
+        <table style={{ fontSize: "10px" }}>
+          <tr>
+            <th>blockNumber</th>
+            <th>fileId</th>
+            <th>fileOwner</th>
+          </tr>
+          {registerEvents.map((event) => {
+            return (
+              <tr>
+                <td>{event.blockNumber}</td>
+                <td>{event.args[0]}</td>
+                <td>{event.args[1]}</td>
+              </tr>
+            );
+          })}
+        </table>
+      )}
+      <h4>AccessGranted</h4>
+      {accessEvents && (
+        <table style={{ fontSize: "10px" }}>
+          <tr>
+            <th>blockNumber</th>
+            <th>fileId</th>
+            <th>fileOwner</th>
+          </tr>
+          {accessEvents.map((event) => {
+            return (
+              <tr>
+                <td>{event.blockNumber}</td>
+                <td>{event.args[0]}</td>
+                <td>{event.args[1]}</td>
+              </tr>
+            );
+          })}
+        </table>
+      )}
+    </>
+  );
+}
