a5cf28f6a6635795fccf7a78c0a7ad52463ccece
Jorge Olmos <jorgeolmos@gmail.com>
2022-07-25 19:23:18 -0300

    Temporal hardcoded env for XMTP

--- src/utils/xmtp/xmtp-env.js
+++ src/utils/xmtp/xmtp-env.js
@@ -1,5 +1,7 @@
 export const getXmtpEnv = () => {
     return process.env.NEXT_PUBLIC_XMTP_ENVIRONMENT === "production"
     ? "production"
-    : "dev"
+    // temporal hardcoded to get production in place, not access to envs vars in vercel ATM
+    : "production"
+    // : "dev"
 }
