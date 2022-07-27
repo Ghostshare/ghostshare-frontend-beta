cd2eccafe4e7fd52c3ce05702ea35e1517983cce
Joris Zierold <contact@joriszierold.com>
2022-07-27 13:43:29 +0100

    add content WHAT section

--- components/WhatWhyHow.jsx
+++ components/WhatWhyHow.jsx
@@ -1,4 +1,8 @@
+import React from "react";
 import { Typography, Container, Box } from "@mui/material";
+import AddTaskIcon from "@mui/icons-material/AddTask";
+import EnhancedEncryptionIcon from "@mui/icons-material/EnhancedEncryption";
+import PublicIcon from "@mui/icons-material/Public";
 
 const styles = {
   Box: {
@@ -14,22 +18,62 @@ const styles = {
     top: 0,
     boxShadow: "0px -6px 10px 0px #00000052",
   },
+  title: {
+    fontSize: "3rem",
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: "50px",
+  },
+  benefitBox: {
+    marginBottom: "50px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    textAlign: "center",
+  },
+  subtitleBox: { display: "flex", marginBottom: "25px" },
+  subtitleIcon: { fontSize: "3rem", marginRight: "15px" },
+  subtitle: { fontSize: "2rem", fontWeight: "bold" },
 };
 
 const WhatWhyHow = () => {
   return (
     <>
       <Box id="what" sx={styles.Box}>
-        <Container maxWidth="md">
-          <Typography variant="h1" style={{ whiteSpace: "pre-line" }}>
-            What you can expect.
-          </Typography>
-          <Typography>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
-          </Typography>
+        <Container maxWidth="sm">
+          <Typography style={styles.title}>What you can expect.</Typography>
+          <Box sx={styles.benefitBox}>
+            <Box sx={styles.subtitleBox}>
+              <AddTaskIcon sx={styles.subtitleIcon} />
+              <Typography sx={styles.subtitle}>Hassle-free</Typography>
+            </Box>
+            <Typography>
+              Even though it’s a Web3 Application, we hide everything what’s no
+              needed away from you for the best sharing experience. No wallet,
+              no gas!
+            </Typography>
+          </Box>
+          <Box sx={styles.benefitBox}>
+            <Box sx={styles.subtitleBox}>
+              <EnhancedEncryptionIcon sx={styles.subtitleIcon} />
+              <Typography sx={styles.subtitle}>100% Private</Typography>
+            </Box>
+            <Typography>
+              By using our service, you don’t expose your email address or any
+              other personal data. Every file share happens in secret.
+            </Typography>
+          </Box>
+          <Box sx={styles.benefitBox}>
+            <Box sx={styles.subtitleBox}>
+              <PublicIcon sx={styles.subtitleIcon} />
+              <Typography sx={styles.subtitle}>Decentralized</Typography>
+            </Box>
+            <Typography>
+              To store data, we use censorship resistant public peer-to-peer
+              networks, instead of centralized servers which can be a single
+              point of failure.
+            </Typography>
+          </Box>
         </Container>
       </Box>
 
