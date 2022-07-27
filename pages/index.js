5aa09ae857e285864b81d037c93d24794222eb0c
Joris Zierold <contact@joriszierold.com>
2022-07-27 14:47:01 +0100

    update title and description

--- pages/index.js
+++ pages/index.js
@@ -24,8 +24,8 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Ghostshare</title>
-        <meta name="description" content="Private File Transfer. Simplified." />
+        <title>GhostShare</title>
+        <meta name="description" content="Private File Sharing. Simplified." />
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👻</text></svg>"
